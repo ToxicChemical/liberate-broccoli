8365667c12b233a8f0c605e1c819954d02b082f3
Фима Фирсова <117814203+ToxicChemical@users.noreply.github.com>
2022-12-05 23:42:35 +0300

    Update Excel_Read.py

--- proj/Excel_Read.py
+++ proj/Excel_Read.py
@@ -13,24 +13,27 @@ def plotfile(data, x_name, y_name):
         for i in df.columns:
             for j in df[i].tolist():
                 print(str(j), file=f)'''
+def execute_Excel_Read():
+    print('Число графиков:')
+    plot_num = int(input())  #Ввод числа графиков (опционально)
 
-
-print('Число графиков:')
-plot_num = int(input())  #Ввод числа графиков (опционально)
-
-print('Путь сохранения:')
-path = input()  # Ввод пути папки сохранения
-# path = '/home/fima/Documents/uni/labs/' + lab_num + '/figs/graph_' + graph_num + '.png'
-data = pd.read_excel("name" + ".xlsx")
-QuestionsGraphs()
-# gystfile(data)
-for i in range(plot_num):
-    print('XY-координаты' + str(i) + '-го графика')
-    x, y = input()   # Ввод x,y координат i-го графика
-    plotfile(data, x, y)
-    # tablefile(data)
-    PlotBuild()
-PlotShow(path)
+    print('Путь сохранения:')
+    path = input()  # Ввод пути папки сохранения
+    # path = '/home/fima/Documents/uni/labs/' + lab_num + '/figs/graph_' + graph_num + '.png'
+    print('Имя .xlsx файла(data по default(оставьте строчку пустой))')
+    name = input()
+    if (name == ''):
+        name = 'data'
+    data = pd.read_excel(name + ".xlsx")
+    QuestionsGraphs()
+    # gystfile(data)
+    for i in range(plot_num):
+        print('XY-координаты' + str(i) + '-го графика(через пробел, названия не содержат пробелы)')
+        x, y = input().split()   # Ввод x,y координат i-го графика
+        plotfile(data, x, y)
+        # tablefile(data)
+        PlotBuild()
+    PlotShow(path)
 # print('Вид таблиции:')
 # table_name = input()
 # TableBuild(data, tablename)
