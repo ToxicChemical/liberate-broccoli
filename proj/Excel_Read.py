f86d749f2510f09f37dc3f096ebdbee8b111093b
Фима Фирсова <117814203+ToxicChemical@users.noreply.github.com>
2022-12-05 23:18:20 +0300

    :|

--- proj/Excel_Read.py
+++ proj/Excel_Read.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
+from Graphs import *
 
 def plotfile(data, x_name, y_name):
     df = pd.concat([data[x_name], data[y_name]], sort=False, axis=1)
@@ -7,19 +8,29 @@ def plotfile(data, x_name, y_name):
         for i in range(len(df)):
             print(df.iloc[i][x_name], df.iloc[i][y_name], file=f, sep=' ')
 
-def tablefile(df):
+'''def gystfile(df):
     with(open('tablefile', 'w') as f):
         for i in df.columns:
-            s = ''
             for j in df[i].tolist():
-                s += ' & ' + str(j)
-            print(i + s, file=f)
+                print(str(j), file=f)'''
 
-plot_num = int(input())
-path = input()
+
+print('Число графиков:')
+plot_num = int(input())  #Ввод числа графиков (опционально)
+
+print('Путь сохранения:')
+path = input()  # Ввод пути папки сохранения
+# path = '/home/fima/Documents/uni/labs/' + lab_num + '/figs/graph_' + graph_num + '.png'
 data = pd.read_excel("name" + ".xlsx")
+QuestionsGraphs()
+# gystfile(data)
 for i in range(plot_num):
-    x, y = input()
+    print('XY-координаты' + str(i) + '-го графика')
+    x, y = input()   # Ввод x,y координат i-го графика
     plotfile(data, x, y)
-    tablefile(data)
-    # Код по графикам
+    # tablefile(data)
+    PlotBuild()
+PlotShow(path)
+# print('Вид таблиции:')
+# table_name = input()
+# TableBuild(data, tablename)
