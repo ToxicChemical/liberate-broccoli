d62c0b11d12f0500bc01fb216573b649d4057819
Фима Фирсова <117814203+ToxicChemical@users.noreply.github.com>
2022-12-05 23:43:33 +0300

    Update heart.py

--- proj/heart.py
+++ proj/heart.py
@@ -4,38 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pandas
 import csv
-from Build_Graph.py import *
-from Excel_Read.py import *
+#from Build_Graph.py import *
+from Excel_Read import *
 from Transform_to_Latex import *
 
 
 
 print('what will we build?')
-print('graphics = a, tables = b, histograms = c')
+print('graphics = a, tables = b, (histograms = c)')
 desired = input()
 
 if desired == 'a':
-    print('Число графиков:')
-    plot_num = int(input())  #Ввод числа графиков (опционально)
-    print('Путь сохранения:')
-    path = input()  # Ввод пути папки сохранения
-    # path = '/home/fima/Documents/uni/labs/' + lab_num + '/figs/graph_' + graph_num + '.png'
-    data = pd.read_excel("name" + ".xlsx")
-    
-    Questions()
-    
-    for i in range(plot_num):
-        print('XY-координаты' + str(i) + '-го графика')
-        x, y = input()   # Ввод x,y координат i-го графика
-        plotfile(data, x, y)
-        # tablefile(data)
-        PlotBuild()
-    PlotShow(path)
-    
-elif desired == 'b':
-    
-elif desired == 'c':  
-      
+    execute_Excel_Read()
+elif desired == 'b':  
     read_csv(file_name)
     make_latex_table_with_borders(data)
     make_latex_table_without_borders(data)
@@ -45,13 +26,3 @@ elif desired == 'c':
     len_array = [int(input()) for _ in range(3)]
     
     make_latex_table_with_length_columns(data, len_array)
-    '''
-    :param data:
-    :param len_array: ширина столбца в сантиметрах
-    :return:
-    '''
-    '''len_array = [2, 3, 4]
-    A = read_csv(r"C:\Users\user\Downloads\23.csv")
-    for i in make_latex_table_with_length_columns(A, len_array):
-        print(i)         '''
-
